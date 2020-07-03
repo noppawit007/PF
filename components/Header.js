7516c14491fc5fdd8ee1d422533c3983a58a68cd
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-03 19:23:41 +0700

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+const Header = () => {
+    return (
+        <div>
+            <Head>
+                <title>Profile</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+               
+            </Head>
+        </div>
+    )
+
+}
+export default Header
