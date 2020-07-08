24aeb1e8b06d8e1659cb6f68a689de85c51d6ea5
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-08 20:10:11 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
             <Head>
                 <title>Profile</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-               
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700" />
             </Head>
         </div>
     )
