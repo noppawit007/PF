dfef485d057f2f992b4daf952a5738a56abf6184
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-09 20:36:30 +0700

    Create Login.js

---
+++ components/Login.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+const Login = () => {
+    return (
+        <div>
+            
+        <div class="container">
+            <div class="contact_area">
+                <h1>Login</h1>
+                <form>
+                    <label>Username</label><br></br>
+                    <input type="text" placeholder="Username"></input><br></br>
+                    <label>Password</label><br></br>
+                    <input type="Password" placeholder="Password"></input><br></br>
+                    <button>send</button><br></br>
+                </form>
+            </div>
+        </div>
+    
+            
+            
+        </div>
+    )
+}
+export default Login
