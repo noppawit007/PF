bc3b6166452577dff44925ccdb8e61f331483064
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-09 20:36:20 +0700

    Create Content.js

---
+++ components/Content.js
@@ -0,0 +1,22 @@
+const Content = () => {
+    return (
+        <div>
+        <p class="wit">This is Me !</p><br></br>
+        <div class="pic1">
+        <img 
+                    src="https://scontent.furt3-1.fna.fbcdn.net/v/t1.0-9/14947968_1835723056647147_1229263366595069170_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_eui2=AeEKk9DsYHYBeAFFAp-QnPrpQxpeN7cMKltDGl43twwqW2bn79Ams9k4GlFvmqdNVDndxno7W-ClTIJgpQgyDCe0&_nc_oc=AQksx7gocg-2lefIGaS4INuyBYsuWUonFE-ewpKtPsMTMhG-xFyxt-WTWNPvkp_zLUM&_nc_ht=scontent.furt3-1.fna&oh=9a234d841adc4c896633c07a0b0b796f&oe=5F2AA4C9"
+                    alt="Me" width="250" height="200" />
+        </div>
+        <div>
+          <p class="facebook"><a href="https://www.facebook.com/nopphawit.nakkhaban.7" target="_blank">Facebook</a></p>
+        </div>
+        <br></br>
+        <div class="bo1">My name is Nopphawit Nakkhaban</div><br></br>
+        <div class="bo2">My Sudentid is 5735512094</div><br></br>
+        <div class="bo3">My Faculty is Computer Engineering</div><br></br>
+        <div class="bo4">My Nickname is Wit</div><br></br>
+        <div class="bo5">Phone is 0960063334</div><br></br>
+        </div>
+    )
+}
+export default Content
