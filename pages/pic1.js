ffe620c3b5b2acfefe44100ca543ccab16edfef2
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-10 18:14:29 +0700

    Update pic1.js

--- pages/pic1.js
+++ pages/pic1.js
@@ -1,18 +1,27 @@
+import Navbar from '../components/Nev'
+import Header from '../components/Header'
 
-export default function Home ()  {
-
-    const myarray = i => <div>pic {i} </div>
-    let url = ['https://cdn.stocksnap.io/img-thumbs/960w/tropical-beach_9FM2TZZYP4.jpg','https://cdn.stocksnap.io/img-thumbs/960w/beach-aerial_NPCFR4GDNF.jpg']
+const pic1 = () => {
+   
+  
+    let url = [
+        { image : "https://cdn.pixabay.com/photo/2020/06/02/05/39/barley-5249498__340.jpg"},
+        { image : 'https://cdn.stocksnap.io/img-thumbs/960w/beach-aerial_NPCFR4GDNF.jpg'}, 
+        { image : 'https://cdn.pixabay.com/photo/2020/07/04/06/40/clouds-5368435__340.jpg'},
+        { image : 'https://cdn.pixabay.com/photo/2019/08/06/12/14/chile-4388204__340.jpg'}
+    ]
     return (
         <div>
-            Hello
+            <Header/>
+            <Navbar/>
             <ul>
             {
-                url.map(item => {
-                    return (<li>{item}</li>)
-                })
+                url.map(item => (
+                    <li><img src ={item.image} alt="Me" width="250" height="200"/></li> )
+                )
             }
             </ul>
         </div>
     )
 }
+export default pic1
