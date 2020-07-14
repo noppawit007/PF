b583bb43f6ffe24c288459bbdf4bdbd9551a8b39
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-14 19:46:21 +0700

    Create test4.js

---
+++ pages/test4.js
@@ -0,0 +1,15 @@
+import T5 from './test5'
+
+const test4 = ()=> {
+     
+    const name ="jgkgh"
+     return (
+         
+        <div>
+         <T5 name =<name> />
+         
+         </div>
+     )
+    
+}
+export default test4
