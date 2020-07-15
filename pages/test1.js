44db01289ac9ce8023178f62c924d6bd63d87ada
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-15 21:39:54 +0700

    Update test1.js

--- pages/test1.js
+++ pages/test1.js
@@ -1,15 +1,25 @@
 import T2 from './test2'
 import T3 from './test3'
+import {useState} from 'react'
+
+export default function Home () {
+ const [number,setNumber] = useState('0')
 
 const test1 = () => {
     const name = 'jk'
     const surname = 'gj'
+function handleClick {x}{
+  setNumber(x)
+}
 
   return (
     <div>
         {''}
       test : {name}-{surname}
+Number State : {number}  
+<button onClick={setNumber(1)}>Click</button>  
       <T2 
+
         name={name} 
 
         surname={surname} />
@@ -17,5 +27,6 @@ const test1 = () => {
         <T3 n="xxx" />
     </div>
   )
+}
 }
 export default test1
