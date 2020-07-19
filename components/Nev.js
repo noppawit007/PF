35b37ee833a537c8e1f7ac5d6d4fbd7b6b8d64ba
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-20 06:03:44 +0700

    Update Nev.js

--- components/Nev.js
+++ components/Nev.js
@@ -8,19 +8,18 @@ const Navbar = () => {
 
       <div class="navbar">
         <Link href="index">Home</Link>
-        <Link href="#">Aboutme</Link>
-        <Link href="#">Contact</Link>
+        <Link href="aboutme">Aboutme</Link>
+        <Link href="contact">Contact</Link>
         <Link href="Login">Login</Link>
         <div class="dropdown">
           <button class="dropbtn">Picture</button>
           <div class="dropdown-content">
-            <Link href="pic1">Nature</Link>
-            <Link href="pic2">Flowers</Link>
-            <Link href="pic3">Picture3</Link>
+            <Link href="Pic1">Gallery</Link>
+            
           </div>
         </div>
       </div><br></br>
-    </div>
+      </div>
   )
 }
 export default Navbar
