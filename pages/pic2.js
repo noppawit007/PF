eeb0f6c50da139e37cce7ebdef9e40c9d6f3bdc5
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-20 06:04:04 +0700

    Update pic2.js

--- pages/pic2.js
+++ pages/pic2.js
@@ -1,27 +1,25 @@
-import Navbar from '../components/Nev'
-import Header from '../components/Header'
-
-const pic2 = () => {
-   
-  
-    let url = [
-        { image : "https://cdn.pixabay.com/photo/2012/03/01/00/55/flowers-19830__340.jpg"},
-        { image : 'https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819__340.jpg'}, 
+export default ({ url }) => {
+    let rl = [
+        { image : "https://cdn.pixabay.com/photo/2014/02/27/16/10/spring-276014__340.jpg"},
+        { image : 'https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228__340.jpg'}, 
+        { image : 'https://cdn.pixabay.com/photo/2018/10/30/16/06/water-lily-3784022__340.jpg'},
         { image : 'https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509__340.jpg'},
-        { image : 'https://cdn.pixabay.com/photo/2018/01/28/11/24/sunflower-3113318__340.jpg'}
+        
     ]
     return (
         <div>
-            <Header/>
-            <Navbar/>
+           <div class = "flex-container" >           
+            
             <ul>
             {
-                url.map(item => (
-                    <li><img src ={item.image} alt="Me" width="250" height="200"/></li> )
+                rl.map(item => (
+                    <li><img src ={item.image} alt="" width="400" height="400"/></li> )
                 )
             }
             </ul>
+            </div>
+
         </div>
     )
+
 }
-export default pic2
