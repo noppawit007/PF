a37e5d1eda2cdacb718ee8766155a0a2c88f7264
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-20 06:03:40 +0700

    Create FistPage.js

---
+++ components/FistPage.js
@@ -0,0 +1,24 @@
+const FistPage = () => {
+    return (
+        <div>
+     
+        <div class="container">
+           
+            <img 
+                    src="https://cdn.pixabay.com/photo/2017/02/23/09/22/tulips-2091609__340.jpg"
+                    alt="Me" width="850" height="400" />
+            
+                <div class="info1_text">
+                    <h1>ยินดีต้อนรับสู่เว็บไซต์แสดงภาพ</h1><br></br>
+                  
+                    </div>
+                    
+              
+              
+            </div>
+        </div>
+    
+        
+    )
+}
+export default FistPage
