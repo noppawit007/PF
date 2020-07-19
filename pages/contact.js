f16d9aa91739816fbabe413d9ec62b1414f73013
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-20 06:03:57 +0700

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+import Navbar from '../components/Nev'
+import Header from '../components/Header'
+import Content from '../components/Content'
+import Footer from '../components/Footer'
+import Login from '../components/Login'
+import Contact from '../components/Contact'
+import Head from 'next/head'
+
+export default () => {
+  return (
+    <div>
+      <Header />
+      <Navbar/>
+      <Contact/><br></br>
+      
+
+    </div>
+  )
+}
