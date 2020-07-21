8bcdc8fcf00763a24eed6b0f9964d67c4f2cab80
noppawit007 <36735731+noppawit007@users.noreply.github.com>
2020-07-21 22:05:46 +0700

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -9,20 +9,20 @@ const Contact = () => {
                     <h1>Contact Us</h1>
                     <form>
                         <label>Your Name</label><br ></br>
-                        <input type="text" placeholder="Your Name"/><br ></br>
-                            <label>Your Email</label><br></br>
-                                <input type="email" placeholder="Your Email"/><br ></br>
-                                    <label>Your Subject</label><br></br>
-                                        <input type="text" placeholder="Your Subject"/><br ></br>
-                                            <label>Your Message</label><br></br>
-                                                <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea><br></br>
-                                                    <button>send</button><br ></br>
-                </form>
+                        <input type="text" placeholder="Your Name" /><br ></br>
+                        <label>Your Email</label><br></br>
+                        <input type="email" placeholder="Your Email" /><br ></br>
+                        <label>Your Subject</label><br></br>
+                        <input type="text" placeholder="Your Subject" /><br ></br>
+                        <label>Your Message</label><br></br>
+                        <textarea name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea><br></br>
+                        <button>send</button><br ></br>
+                    </form>
+                </div>
             </div>
-        </div>
-    
-            
-            
+
+
+
         </div>
     )
 }
